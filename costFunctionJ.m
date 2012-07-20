e5a1962a9791c59dd5e9d4c97aafc72aa622c708
Sam Joseph <srjoseph@hawaii.edu>
2012-07-20 15:20:22 +0100

    screen scraped extra files from Prof Ngs Octave Tutorial

---
+++ costFunctionJ.m
@@ -0,0 +1,12 @@
+% TODO translate this to Python
+
+function J = costFunctionJ(X, y, theta)
+
+% X is the "design matrix" containing our training examples.
+% y is the class labels
+
+m = size(X,1);          % number of training examples
+predictions = X*theta;  % predictions of hypothesis on all m examples
+sqrErrors = (predictions-y).^2; % squared errors
+
+J = 1/(2*m) * sum(sqrErrors);
