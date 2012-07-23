a07221f97ab65c62c48b3ff9d28fc05a59b4539a
Sam Joseph <srjoseph@hawaii.edu>
2012-07-23 22:53:10 +0100

    added a python start on costFunctionJ without the solution component - also note this isn't tested - not sure it is correct scipy yet ...

---
+++ costFunctionJ.py
@@ -0,0 +1,23 @@
+def costFunctionJ(X, y, theta):
+  #COSTFUNCTION Compute cost and gradient for logistic regression
+  #   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+  #   parameter for logistic regression and the gradient of the cost
+  #   w.r.t. to the parameters.
+
+  # Initialize some useful values
+  m = size(y,1) # number of training examples
+
+  # You need to return the following variables correctly 
+  J = 0
+  grad = zeros(size(theta,1))
+
+  # ====================== YOUR CODE HERE ======================
+  # Instructions: Compute the cost of a particular choice of theta.
+  #               You should set J to the cost.
+  #               Compute the partial derivatives and set grad to the partial
+  #               derivatives of the cost w.r.t. each parameter in theta
+  #
+  # Note: grad should have the same dimensions as theta
+  #
+  
+  return J
