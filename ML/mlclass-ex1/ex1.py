f9d1293c2a66895d4f2b47c10e3f145046d1b76b
Sam Joseph <srjoseph@hawaii.edu>
2012-08-07 16:32:19 +0900

    missed a small syntax issue

--- ML/mlclass-ex1/ex1.py
+++ ML/mlclass-ex1/ex1.py
@@ -83,9 +83,9 @@ legend(('Training data', 'Linear regression'))
 
 # Predict values for population sizes of 35,000 and 70,000
 predict1 = array([1, 3.5]) *theta
-print 'For population = 35,000, we predict a profit of #f\n' % predict1*10000
+print 'For population = 35,000, we predict a profit of %f\n' % predict1.var()*10000
 predict2 = array([1, 7]) * theta
-print 'For population = 70,000, we predict a profit of #f\n' % predict2*10000
+print 'For population = 70,000, we predict a profit of %f\n' % predict2.var()*10000
 
 print 'Program paused. Press enter to continue.\n'
 raw_input()
