8fbc2c3d048d585d3dddb06d59000f46f60f7434
Ioura Batugowski <ibatugow@gmail.com>
2012-09-02 00:49:03 +0200

    adding templates for the other exercises

---
+++ ML/mlclass-ex1/normalEqn.py
@@ -0,0 +1,26 @@
+from numpy import *
+from numpy.linalg import *
+
+def normalEqn(X, y):
+    #NORMALEQN Computes the closed-form solution to linear regression
+    #   NORMALEQN(X,y) computes the closed-form solution to linear
+    #   regression using the normal equations.
+
+    theta = zeros(size(X, 1))
+
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: Complete the code to compute the closed form solution
+    #               to linear regression and put the result in theta.
+    #
+
+    # ---------------------- Sample Solution ----------------------
+
+
+
+
+    # -------------------------------------------------------------
+
+
+    # ============================================================
+
+    return theta
